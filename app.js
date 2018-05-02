91d9b85f17c1ac41cc5f4a9080385c003d505dc6
robincollins <racollins1@me.com>
2018-05-02 23:14:13 +0100

    created more note functions

--- app.js
+++ app.js
@@ -3,21 +3,23 @@ console.log("starting app");
 //3rd party node modules
 const fs = require("fs");
 const _ = require("lodash");
+const yargs = require("yargs");
 // node modules i've created and required
 const notes = require("./notes.js")
 
-
+const argv = yargs.argv;
 var command = process.argv[2];
 console.log("Command: ", command);
+console.log("Yargs", argv);
 
 if (command === "add") {
-  console.log("adding new note");
+  notes.addNote(argv.title, argv.body);
 } else if (command === "list") {
-  console.log("listing all notes");
+  notes.getAll();
 } else if (command === "read") {
-  console.log("fetching note");
+  notes.getNote(argv.title);
 } else if (command === "remove") {
-  console.log("removing note");
+  notes.removeNote(argv.title);
 } else {
   console.log("command not recognised");
 }
--- notes.js
+++ notes.js
@@ -1,6 +1,25 @@
 console.log("starting notes.js");
 
-module.exports.addNote = () => {
-  console.log("addNote");
-  return "New note";
+var addNote = (title, body) => {
+  console.log("Adding note", title, body);
+};
+
+var getAll = () => {
+  console.log("Getting all notes");
+};
+
+var getNote = (title) => {
+  console.log("Getting note", title);
+};
+
+var removeNote = (title) => {
+  console.log("Removing note", title);
+};
+
+module.exports = {
+  addNote,
+  //same as: 'addNote: addNote' on it's own
+  getAll,
+  getNote,
+  removeNote
 };

dab91915c8034538175654a8a79acb3001e6bba0
robincollins <racollins1@me.com>
2018-05-02 22:37:20 +0100

    added if else statement for command line commands using process.argv

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ const fs = require("fs");
 const _ = require("lodash");
 // node modules i've created and required
 const notes = require("./notes.js")
+
+
+var command = process.argv[2];
+console.log("Command: ", command);
+
+if (command === "add") {
+  console.log("adding new note");
+} else if (command === "list") {
+  console.log("listing all notes");
+} else if (command === "read") {
+  console.log("fetching note");
+} else if (command === "remove") {
+  console.log("removing note");
+} else {
+  console.log("command not recognised");
+}

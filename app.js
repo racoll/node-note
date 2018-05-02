a7df0e57e98d1b634893d622b88e9fd367ce6f84
robincollins <racollins1@me.com>
2018-05-02 22:27:07 +0100

    file removal and tidy up

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
 console.log("starting app");
 
+//3rd party node modules
 const fs = require("fs");
-const os = require("os");
-const notes = require("./notes.js")
 const _ = require("lodash");
-
-console.log(_.isString(true));
-console.log(_.isString("Robin"));
-
-var filteredArray = _.uniq(["Jon"]);
-console.log(filteredArray);
-// var res = notes.addNote();
-// console.log(res);
-
-var addition = notes.add(2,5);
-console.log(addition);
-
-// var user = os.userInfo();
-//
-//
-// fs.appendFileSync('greetings.txt', `Hello ${user.username} you are ${notes.age}`);
+// node modules i've created and required
+const notes = require("./notes.js")
--- notes.js
+++ notes.js
@@ -4,7 +4,3 @@ module.exports.addNote = () => {
   console.log("addNote");
   return "New note";
 };
-
-module.exports.add = (num1, num2) => {
-  return (num1 + num2);
-}
